1d049ae6f5af211780d9c1a94e5e2b2760016f3a
Bkliger <bkliger@comcast.net>
2016-04-15 18:40:18 -0700

    before additing edit fields

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -1,7 +1,52 @@
 angular.module('libraryApp')
-  .controller('BooksIndexController', BooksIndexController);
+       .controller('BooksIndexController', BooksIndexController);
 
-BooksIndexController.$inject=['$http'];
-function BooksIndexController( $http ) {
+BooksIndexController.$inject = ['$http', '$routeParams', '$location'];
+
+function BooksIndexController ($http, $routeParams, $location) {
   var vm = this;
+  var endpoint = 'https://super-crud.herokuapp.com/books';
+  vm.newBook = {};
+  vm.newBook = {
+    title: 'Test Book',
+    author: 'Test Author',
+    image: "",
+    releaseDate: "January 30, 1873"
+  };
+
+  $http({
+    method: 'GET',
+    url: endpoint
+  }).then(function successCallback(response) {
+    vm.books = response.data.books;
+  }, function errorCallback(response) {
+    console.log('There was an error getting the data', response);
+  });
+
+  vm.createBook = function () {
+    console.log(vm.newBook);
+    $http({
+      method: "POST",
+      url: endpoint,
+      data: vm.newBook
+    }).then(function successCb(response) {
+      vm.books.push(response.data);
+    },
+    function errorCb(response) {
+      console.log('There was an error creating a book', response);
+    });
+  };
+  vm.deleteBook = function (book) {
+  $http({
+    method: 'DELETE',
+    url: endpoint + "/" + book._id
+  }).then(function successCallback(response) {
+    // $location.path("/");
+    var index = vm.books.indexOf(book);
+    vm.books.splice(index,1)
+
+  }, function errorCallback(response) {
+    console.log('There was an error deleting the data', response);
+  });
+};
 }
--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -1,7 +1,19 @@
 angular.module('libraryApp')
-  .controller('BooksIndexController', BooksIndexController);
+  .controller('BooksShowController', BooksShowController);
 
 BooksShowController.$inject=['$http', '$routeParams', '$location'];
 function BooksShowController($http, $routeParams, $location) {
   var vm = this;
-};
+  var endpoint = 'https://super-crud.herokuapp.com/books';
+  vm.book = {};
+
+  $http({
+    method: 'GET',
+    url: endpoint + "/" + $routeParams.id
+  }).then(function successCallback(response) {
+    console.log(response.data);
+    vm.book = response.data;
+  }, function errorCallback(response) {
+    console.log('There was an error getting the data', response);
+  });
+}
